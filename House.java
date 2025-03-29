e027e134b68d2d58056565b972bf787337eb1e7c
tnphammy <tnpham@smith.edu>
2025-03-28 23:39:07 -0400

    for House: made constructor and hasDiningRoom()

--- House.java
+++ House.java
@@ -1,9 +1,28 @@
-/* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
 
-  public House() {
+/* This is a stub for the House class */
+public class House extends Building implements HouseRequirements {
+  private ArrayList<Student> residents; // The <Student> tells Java what kind of data we plan to store IN the ArrayList
+  private boolean hasDiningRoom;
+
+
+  public House(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    ArrayList<Student> residents = new ArrayList<>(Student);
     System.out.println("You have built a house: 🏠");
   }
+
+  public boolean hasDiningRoom() {
+    if (hasDiningRoom = true) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+
+
 
   public static void main(String[] args) {
     new House();
